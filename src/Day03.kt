e7001bc86be032167949fe2f7af8eab46b7e80cd
Zdeněk Oborník <zdenek@obornik.cz>
2023-12-03 11:28:12 +0100

    Day 3 - Cleanup

--- src/Day03.kt
+++ src/Day03.kt
@@ -5,23 +5,15 @@ fun List<String>.allPositions(): List<Pair<Int, Int>> = this.indices.flatMap { y
 }
 
 fun List<String>.adjacentDigits(y: Int, x: Int) = listOf(
-    y to x - 1,
-    y - 1 to x,
-    y to x + 1,
-    y + 1 to x,
-    y - 1 to x - 1,
-    y - 1 to x + 1,
-    y + 1 to x - 1,
-    y + 1 to x + 1,
+    // directly adjacent
+    y to x - 1, y - 1 to x, y to x + 1, y + 1 to x,
+    // diagonally adjacent
+    y - 1 to x - 1, y - 1 to x + 1, y + 1 to x - 1, y + 1 to x + 1,
 ).filter { (y, x) -> y in 0..this[y].lastIndex && x in 0..this.lastIndex && this[y][x].isDigit() }
 
 fun List<String>.findPossibleNumber(y: Int, x: Int): Number {
-    val startX = (x - 1 downTo 0)
-        .takeWhile { this[y][it].isDigit() }
-        .lastOrNull() ?: x
-    val endX = (x + 1..this[y].lastIndex)
-        .takeWhile { this[y][it].isDigit() }
-        .lastOrNull() ?: x
+    val startX = (x - 1 downTo 0).takeWhile { this[y][it].isDigit() }.lastOrNull() ?: x
+    val endX = (x + 1..this[y].lastIndex).takeWhile { this[y][it].isDigit() }.lastOrNull() ?: x
     val number = this[y].substring(startX..endX).toInt()
 
     return Number(number, y, startX, endX)
