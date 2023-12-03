324e8f7277ada15598aa4838f3111764d7b3e837
Zdeněk Oborník <zdenek@obornik.cz>
2023-12-03 15:16:12 +0100

    Day 3 - minor refactoring

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,17 +1,17 @@
 data class Number(val number: Int, val y: Int, val startX: Int, val endX: Int)
 
-fun List<String>.allPositions(): List<Pair<Int, Int>> = this.indices.flatMap { y ->
+private fun List<String>.allPositions(): List<Pair<Int, Int>> = this.indices.flatMap { y ->
     this[y].indices.map { y to it }
 }
 
-fun List<String>.adjacentDigits(y: Int, x: Int) = listOf(
+private fun List<String>.adjacentPositions(y: Int, x: Int) = listOf(
     // directly adjacent
     y to x - 1, y - 1 to x, y to x + 1, y + 1 to x,
     // diagonally adjacent
     y - 1 to x - 1, y - 1 to x + 1, y + 1 to x - 1, y + 1 to x + 1,
-).filter { (y, x) -> y in 0..this[y].lastIndex && x in 0..this.lastIndex && this[y][x].isDigit() }
+).filter { (y, x) -> y in 0..this[y].lastIndex && x in 0..this.lastIndex }
 
-fun List<String>.findPossibleNumber(y: Int, x: Int): Number {
+private fun List<String>.findFinalNumberFromDigit(y: Int, x: Int): Number {
     val startX = (x - 1 downTo 0).takeWhile { this[y][it].isDigit() }.lastOrNull() ?: x
     val endX = (x + 1..this[y].lastIndex).takeWhile { this[y][it].isDigit() }.lastOrNull() ?: x
     val number = this[y].substring(startX..endX).toInt()
@@ -19,23 +19,21 @@ fun List<String>.findPossibleNumber(y: Int, x: Int): Number {
     return Number(number, y, startX, endX)
 }
 
+private fun List<String>.findAdjacentNumbers(y: Int, x: Int) = adjacentPositions(y, x)
+    .filter { (y, x) -> this[y][x].isDigit() }
+    .map { (y, x) -> findFinalNumberFromDigit(y, x) }
+    .distinct()
+
 fun main() {
 
     fun part1(input: List<String>): Int = input.allPositions()
         .filter { (y, x) -> !input[y][x].isDigit() && input[y][x] != '.' }
-        .sumOf { (y, x) ->
-            input.adjacentDigits(y, x)
-                .map { (y, x) -> input.findPossibleNumber(y, x) }
-                .distinct()
-                .sumOf { it.number }
-        }
+        .sumOf { (y, x) -> input.findAdjacentNumbers(y, x).sumOf { it.number } }
 
     fun part2(input: List<String>): Int = input.allPositions()
         .filter { (y, x) -> input[y][x] == '*' }
         .sumOf { (y, x) ->
-            input.adjacentDigits(y, x)
-                .map { (y, x) -> input.findPossibleNumber(y, x) }
-                .distinct()
+            input.findAdjacentNumbers(y, x)
                 .takeIf { it.size == 2 }
                 ?.map { it.number }
                 ?.reduce { acc, i -> acc * i } ?: 0
